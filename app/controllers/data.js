1a8c83dfa08f10e504301fc1878da70fd5f3c24d
y00z <550823517@qq.com>
2017-07-20 10:48:22 +0800

    写入接口

--- app/controllers/data.js
+++ app/controllers/data.js
@@ -12,10 +12,39 @@ exports.index = function (req, res) {
 
 
 exports.write = function (req, res) {
-    console.log(req.body)
+    var id = req.body._id
+    //提交过来的对象
+    var dataObj = req.body
+    //要存入的对象
+    var _data
+    if (id) {
+        console.log("已经有了")
+        Data.findByid(id, function (err, _data) {
+            if (err) console.log(err)
+            //提交上来的数据，替换掉数据库已经存在的数据。  (新的替换掉旧的)
+            _data = _.extend(_data, dataObj) //第一个旧的，第二个新的。
+            _data.save(function (err, data) {
+                if (err) console.log(err)
+                var result = JSON.stringify(data);
+                return res.send({
+                    success: true,
+                    msg: "厕所服务11",
+                    data: result
+                })
+            })
+        })
+    } else {
+        console.log("新的")
+        _data = new Data(dataObj)
+        _data.save(function (err, data) {
+            if (err) console.log(err)
+            var result = JSON.stringify(data);
+            return res.send({
+                success: true,
+                msg: "厕所服务11",
+                data: result
+            })
+        })
+    }
 
-    return res.send({
-        success: true,
-        msg: "厕所服务11"
-    })
 }
